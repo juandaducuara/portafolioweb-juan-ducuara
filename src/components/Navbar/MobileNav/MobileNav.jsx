8966f63e0cabb15d3a3d699108e41faa3e7b1eed
juandaducuara <jducuara82@gmail.com>
2024-09-03 22:14:25 -0500

    Fix

--- src/components/Navbar/MobileNav/MobileNav.jsx
+++ src/components/Navbar/MobileNav/MobileNav.jsx
@@ -1,36 +1,36 @@
 import React from "react";
 import "./MobileNav.css";
 
-const MobileNav = ({isOpen,toggleMenu}) => {
+const MobileNav = ({ isOpen, toggleMenu }) => {
   return (
-    <>
     <div 
-        className={`mobile-menu ${isOpen ? "active" : ""}`}
-        onClick={toggleMenu}    
+      className={`mobile-menu ${isOpen ? "active" : ""}`}
+      onClick={toggleMenu}    
     >
-        <div className="mobile-menu-container">
-            <img src="./assets/images/Juan Ducuara.png" alt="" className="logo" />
-            <ul>
-            <li>
-              <a className="menu-item">Inicio</a>
-            </li>
-            <li>
-              <a className="menu-item">Habilidades</a>
-            </li>
-            <li>
-              <a className="menu-item">Experiencia laboral</a>
-            </li>
-            <li>
-              <a className="menu-item">Contacto</a>
-            </li>
-            <button className="contact-btn" onClick={() => { }}>
+      <div className="mobile-menu-container">
+        <img src="./assets/images/Juan Ducuara.png" alt="Logo" className="logo" />
+        <ul>
+          <li>
+            <a className="menu-item" href="#seccionInicio">Inicio</a>
+          </li>
+          <li>
+            <a className="menu-item" href="#seccionHabilidades">Habilidades</a>
+          </li>
+          <li>
+            <a className="menu-item" href="#seccionExperienciaLaboral">Experiencia laboral</a>
+          </li>
+          <li>
+            <a className="menu-item" href="#seccionContacto">Contacto</a>
+          </li>
+          <li>
+            <button className="contact-btn" onClick={() => { /* Lógica para el botón */ }}>
               Contactame
             </button>           
-          </ul>
-        </div>    
+          </li>
+        </ul>
+      </div>    
     </div>
-    </>
-  )
+  );
 }
 
-export default MobileNav
+export default MobileNav;
