d95f4e6d620a706e33fcf6e94f68d4113eebd3d4
juandaducuara <jducuara82@gmail.com>
2024-09-03 21:04:44 -0500

    Actualizacion de inforamcion

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,51 +1,51 @@
 export const SKILLS = [
     {
-        title:"Frontend",
-        icon:"./assets/images/interfaz.png",
-        skills:[
-            {skill:"HTML5",percentage:"70%"},
-            {skill:"CSS3",percentage:"60%"},
-            {skill:"JavaScript",percentage:"70%"},
-            {skill:"React.js",percentage:"40%"}            
+        title: "Frontend",
+        icon: "./assets/images/interfaz.png",
+        skills: [
+            { skill: "HTML5", percentage: "70%" },
+            { skill: "CSS3", percentage: "60%" },
+            { skill: "JavaScript", percentage: "70%" },
+            { skill: "React.js", percentage: "40%" }
         ]
     },
     {
-        title:"Backend",
-        icon:"./assets/images/backend.png",
-        skills:[
-            {skill:"Java",percentage:"70%"},
-            {skill:"C#",percentage:"60%"},
-            {skill:"PHP",percentage:"70%"},
-            {skill:"JavaSript",percentage:"60%"}            
+        title: "Backend",
+        icon: "./assets/images/backend.png",
+        skills: [
+            { skill: "Java", percentage: "70%" },
+            { skill: "C#", percentage: "60%" },
+            { skill: "PHP", percentage: "70%" },
+            { skill: "JavaSript", percentage: "60%" }
         ]
     },
     {
-        title:"Herramientas",
-        icon:"./assets/images/herramientas.png",
-        skills:[
-            {skill:"Visual Studio & VS Code",percentage:"80%"},
-            {skill:"Git & GitHub",percentage:"80%"},
-            {skill:"NetBeans",percentage:"80%"},
-            {skill:"IntelliJ IDEA",percentage:"70%"}            
+        title: "Herramientas",
+        icon: "./assets/images/herramientas.png",
+        skills: [
+            { skill: "Visual Studio & VS Code", percentage: "80%" },
+            { skill: "Git & GitHub", percentage: "80%" },
+            { skill: "NetBeans", percentage: "80%" },
+            { skill: "IntelliJ IDEA", percentage: "70%" }
         ]
     },
     {
-        title:"Soft Skills",
-        icon:"./assets/images/habilidades-blandas.png",
-        skills:[
-            {skill:"Resolucion de problemas",percentage:"80%"},
-            {skill:"Colaboración",percentage:"90%"},
-            {skill:"Comunicacion",percentage:"80%"},
-            {skill:"Liderazgo",percentage:"90%"}            
+        title: "Soft Skills",
+        icon: "./assets/images/habilidades-blandas.png",
+        skills: [
+            { skill: "Resolucion de problemas", percentage: "80%" },
+            { skill: "Colaboración", percentage: "90%" },
+            { skill: "Comunicacion", percentage: "80%" },
+            { skill: "Liderazgo", percentage: "90%" }
         ]
     },
 ]
 
 export const WORK_EXPERIENCE = [
     {
-        title:"Desarrollador Junior - AOA Colombia",
-        date:"Agosto 2023 - Febrero 2024",
-        responsabilities:[
+        title: "Desarrollador Junior - AOA Colombia",
+        date: "Agosto 2023 - Febrero 2024",
+        responsabilities: [
             "Levantamiento de información y Análisis de requerimientos",
             "Desarrollo Web PHP con Framework Laravel",
             "Administración de bases de datos MySQL",
@@ -53,31 +53,29 @@ export const WORK_EXPERIENCE = [
         ]
     },
     {
-        title:"Desarrollador Junior - AOA Colombia",
-        date:"Agosto 2023 - Febrero 2024",
-        responsabilities:[
-            "Levantamiento de información y Análisis de requerimientos",
-            "Desarrollo Web PHP con Framework Laravel",
-            "Administración de bases de datos MySQL",
-            "Soporte a aplicativos (Control operativo, Administrativo)"
+        title: "NyGSoft - Desarrollador JR Full Stack",
+        date: "Mayo 2024  - Actualmente",
+        responsabilities: [
+            "Desarrollo frontend Vue, Bootstrap y jQuery",
+            "Desarrollo backend PHP con framework como CodeIgniter y Laravel y C# con .NET",
+            "Administración de bases de datos MySQL, SQL y Oracle"
         ]
     },
     {
-        title:"Desarrollador Junior - AOA Colombia",
-        date:"Agosto 2023 - Febrero 2024",
-        responsabilities:[
-            "Levantamiento de información y Análisis de requerimientos",
-            "Desarrollo Web PHP con Framework Laravel",
+        title: "SICTAX.NET S.A.S - Desarrollador JR Full Stack",
+        date: "Junio 2024 - Actualmente",
+        responsabilities: [
+            "Desarrollo frontend Bootstrap",
+            "Desarrollo backend PHP con Framework CodeIgniter",
             "Administración de bases de datos MySQL",
-            "Soporte a aplicativos (Control operativo, Administrativo)"
         ]
     },
 ]
 export const ESTUDIOS_REALIZADOS = [
     {
-        title:"Tecnólogo Análisis y Desarrollo de sistemas de informacion SENA",
-        date:"Enero 2022 - Febrero 2024",
-        responsabilities:[
+        title: "Tecnólogo Análisis y Desarrollo de sistemas de informacion SENA",
+        date: "Enero 2022 - Febrero 2024",
+        responsabilities: [
             "Levantamiento de información y Análisis de requerimientos",
             "Desarrollo Web PHP , JAVA , Node.js y C# (.NET)",
             "Administración de bases de datos MySQL",
@@ -87,22 +85,24 @@ export const ESTUDIOS_REALIZADOS = [
         ]
     },
     {
-        title:"Curso - Fundamentos profesionales del desarrollo de software - Microsoft y LinkedIn ",
-        date:"Marzo 2023",
-        responsabilities:[
+        title: "Java y Spring Boot G6 - Oracle Next Education - alura(2024)",
+        date: "Febrero 2024",
+        responsabilities: [
+            "Spring Boot 3: desarrollar una API Rest en Java - alura",
+            "Spring Boot: persistencia de datos y consultas con Spring Data JPA",
+            "Spring Boot 3: documentar, probar y preparar una API para su implementación",
+            "Java: creando tu primera API y conectándola al Front End ",
+            "Git y GitHub: repositorio, commit y versiones"
+        ]
+    },
+    {
+        title: "Curso - Fundamentos profesionales del desarrollo de software - Microsoft y LinkedIn ",
+        date: "Marzo 2023",
+        responsabilities: [
             "Fundamentos esenciales de la programación",
             "Introducción a las habilidades profesionales en el desarrollo de software",
             "Fundamentos de la programación"
         ]
     },
-    {
-        title:"Lógica de programación con JavaScript - alura LATAM",
-        date:"Febrero 2024",
-        responsabilities:[
-            "Trello",
-            "Lógica de programación: JavaSript ",
-            "HTML5 y CSS3 : Avanzado en CSS",
-            "Git y GitHub: Repositorio, commit y versiones"
-        ]
-    },
+
 ]
